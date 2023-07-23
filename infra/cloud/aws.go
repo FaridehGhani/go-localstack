65da773339435a8c30f3665638d98ddf162fc36b
FaridehGhani <faridehghani98@gmail.com>
2023-07-23 08:38:54 +0330

    aws new session

--- infra/cloud/aws.go
+++ infra/cloud/aws.go
@@ -1,1 +1,31 @@
 package cloud
+
+import (
+	"log"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+func NewAWS() *session.Session {
+	var newAWSSession *session.Session
+	var err error
+	var once sync.Once
+
+	once.Do(func() {
+		newAWSSession, err = session.NewSession(
+			&aws.Config{
+				Endpoint:    aws.String("http://localhost:4566"),
+				Region:      aws.String("us-east-1"),
+				Credentials: credentials.NewStaticCredentials("dummy", "dummy", ""),
+			},
+		)
+		if err != nil {
+			log.Fatalf("failed to create aws session: %v", err)
+		}
+	})
+
+	return newAWSSession
+}
